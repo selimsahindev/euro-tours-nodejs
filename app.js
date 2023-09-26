17bb9e87ba8ca9b73e1878e07d430fb2eca88bcc
selimsahindev <selimsahin.sns@gmail.com>
2023-09-26 03:18:59 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  res.send("Done");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
