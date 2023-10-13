4973ec29dc8e69d14ba3a1de9680197e81e11056
selimsahindev <selimsahin.sns@gmail.com>
2023-10-13 23:05:30 +0300

    added filtering by query parameters for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build the query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const queryStr = JSON.stringify(queryObj).replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`,
+    );
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // Execute the query
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
