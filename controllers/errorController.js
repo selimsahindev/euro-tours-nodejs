e65f00cce2d08cb58e93554599f1a1756110ac51
selimsahindev <selimsahin.sns@gmail.com>
2023-10-20 23:48:42 +0300

    unhandled rejections and catching uncaught exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,7 +7,7 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-const sendErrorProd = (req, res) => {
+const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-require('dotenv').config({ path: './config.env' });
-const app = require('./app');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// This should be at the top of the code.
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
@@ -8,10 +17,18 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => console.log('DB connection successful!'));
+  .then((con) => console.log('DB connection successful!'))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server is running on port 3000');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
