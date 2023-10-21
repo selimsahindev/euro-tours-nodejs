c81e558669281d7d3708c60c41da41cd47617e53
selimsahindev <selimsahin.sns@gmail.com>
2023-10-21 22:36:41 +0300

    user login, jwt authentication and authController refactoring

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'Get all users route is not yet defined',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 const getUser = (req, res) => {
   res.status(500).json({
