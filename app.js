4f75f5098d71af73e366f78fd0bac94814272b56
selimsahindev <selimsahin.sns@gmail.com>
2023-10-24 18:21:18 +0300

    installed helmet to set the security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,29 +11,36 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // Global Middlewares
+// Set security HTTP headers.
+app.use(helmet());
 
+// Development logging.
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP.
 const limiter = rateLimit({
   max: 100, // 100 requests per hour.
   windowMs: 60 * 60 * 1000, // In milliseconds.
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 // Apply the limiter only to the /api route.
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser, reading data from body into req.body.
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files.
 app.use(express.static(`${__dirname}/public`));
 
-// A custom middleware which logs the request time
+// A custom middleware which logs the request time.
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Bind the routers to the app
+// Bind the routers to the app.
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
