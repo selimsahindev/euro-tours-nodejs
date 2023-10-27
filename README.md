28dd21ec6ed1b156593560334fa2924816ecb083
selimsahindev <selimsahin.sns@gmail.com>
2023-10-27 22:20:27 +0300

    calculating average rating for tours when adding deleting and updating its reviews

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Then execute with --import to seed the db from the JSON data.
 This will import tours, users and reviews.
 (Don't forget to comment out the password encryption part in userModel.js)
 
-Admin User:
-admin@eurotours.io
-test1234
+- Admin User:
+  admin@eurotours.io
+
+- Password is 'test1234' for all users.
 
 node ./test-data/data/import-dev-data.js --delete
 node ./test-data/data/import-dev-data.js --import
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -39,6 +40,51 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  // Check if there are any reviews for the tour.
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5, // Default value
+    });
+  }
+};
+
+reviewSchema.post('save', function (next) {
+  this.constructor.calcAverageRatings(this.tour);
+  next();
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // Assign the current review to the query object
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed.
+  // Instead, we use the review that we saved in the pre middleware.
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
