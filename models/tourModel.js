0c4575e667a8fbef3d28976cf06b3c8c48c1d244
selimsahindev <selimsahin.sns@gmail.com>
2023-10-27 21:34:33 +0300

    improved tours read performance by adding indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -124,6 +124,9 @@ const tourSchema = mongoose.Schema(
   },
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // Virtual properties
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
