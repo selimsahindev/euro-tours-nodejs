12fe3be95ddfa3b857f531ee2c32d626e82ae5b9
selimsahindev <selimsahin.sns@gmail.com>
2023-10-29 20:42:05 +0300

    tour details page title

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -22,7 +22,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   }
 
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `The ${tour[0].name} Tour`,
     tour: tour[0],
   });
 });
