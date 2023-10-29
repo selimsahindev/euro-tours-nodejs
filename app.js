df41ece50ad950c37a92de208de4ceb069480eae
selimsahindev <selimsahin.sns@gmail.com>
2023-10-29 14:32:03 +0300

    viewRoutes and viewController

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const path = require('path');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -72,12 +74,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use('/', (req, res, next) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Selim Sahin',
-  });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
