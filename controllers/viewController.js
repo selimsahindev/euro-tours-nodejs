02f557eb15cc1f9664d8128db5c9ee339e9c945d
selimsahindev <selimsahin.sns@gmail.com>
2023-10-29 20:40:46 +0300

    tour details page, pug component refactoring

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,27 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.find({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  if (!tour || tour.length === 0) {
+    return renderErrorPage(res, 'Something went wrong', 'Tour not found');
+  }
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour: tour[0],
   });
-};
+});
+
+function renderErrorPage(res, error, description) {
+  return res.status(200).render('error', {
+    title: 'Error',
+    error: error,
+    description: description,
+  });
+}
